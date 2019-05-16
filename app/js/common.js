445c3e2e8683694fa389feec8acfb87a0dd92268
Nesterenko Aleksandr <NesterenkoAA@HQ.ROOT.AD>
2019-05-16 14:57:46 +0300

    Added project files

---
+++ app/js/common.js
@@ -0,0 +1,27 @@
+$(function() {
+	// Adding handler on loading posts button
+	$('.loading-posts').on('click', function() {
+		const btn = $(this);
+		const loader = btn.find('.spinner-border');
+		$.ajax({
+			url: '/data.html',
+			type: 'GET',
+			beforeSend: function() {
+				btn.attr('disabled', true);
+				loader.addClass('d-inline-block');
+			},
+			success: function(response) {
+				setTimeout(function(){
+					loader.removeClass('d-inline-block');
+					btn.attr('disabled', false);
+					$('.after-posts').before(response);
+				}, 1000);
+			},
+			error: function() {
+				loader.removeClass('d-inline-block');
+				btn.attr('disabled', false);
+				alert('An error occurred while trying to load posts!!!');
+			}
+		});
+	});
+});
